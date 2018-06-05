c61ce7258af1b60bedc82fadd5e068ad5c1a7949
w0nd3r0us <steven.grewal@gmail.com>
2018-06-05 17:34:49 -0400

    completed 2nd task

--- exercise.rb
+++ exercise.rb
@@ -1,5 +1,14 @@
-emotions = {
-happiness: rand(1..3),
-sadness: rand(1..3),
-fear: rand(1..3)
-}
+class Person
+
+  @@emotions = {
+    happiness: rand(1..3),
+    sadness: rand(1..3),
+    fear: rand(1..3)
+  }
+
+  def initialize(name)
+    @name = name
+    @emotions = @@emotions
+  end
+
+end
