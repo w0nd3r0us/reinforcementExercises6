e9e3b0eedfc96df4dd93770065f4186ad5aa48b0
w0nd3r0us <steven.grewal@gmail.com>
2018-06-05 17:37:23 -0400

    completed last task

--- exercise.rb
+++ exercise.rb
@@ -9,6 +9,22 @@ class Person
   def initialize(name)
     @name = name
     @emotions = @@emotions
+
   end
+
+  def emotion
+    @emotions.each do |emotion, level|
+      if level == 1
+        puts "I am feeling a low amount of #{emotion}."
+      end
+      if level == 2
+        puts "I am feeling a medium amount of #{emotion}."
+      end
+      if level == 3
+        puts "I am feeling a high amount of #{emotion}."
+      end
+    end
+  end
+
 
 end
